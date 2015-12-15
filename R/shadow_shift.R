51d34cf7c447757b823ef6eb0be5775e01dbcfcb
Nicholas Tierney <nicholas.tierney@gmail.com>
2015-12-15 18:42:28 +1000

    added `df` data, updated comments in `shadow_shift`

--- R/shadow_shift.R
+++ R/shadow_shift.R
@@ -8,19 +8,20 @@
 #'
 #' @export
 
-# Make a window function that transforms missing values to be 10% below the minimum value for that variable
+# shadow_shift is a window function that transforms missing values to be about 10% below the minimum value for a given variable, plus some jittered noise, to separate repeated values, so that missing values can be visualised along with the rest of the data
+
 shadow_shift <- function(x){
-  # Check that x is numeric
-  #
-  xrange <- max(x, na.rm = T)-min(x, na.rm = T)
+
+  xrange <- max(x, na.rm = T) - min(x, na.rm = T)
+
   xmin <- min(x, na.rm = T)
+
+  # create the "jitter" to be added around the points.
   xrunif <- (runif(length(x))-0.5)*xrange*0.05
+
   ifelse(is.na(x),
+         # add the jitter around the those values that are missing
          yes = xmin-xrange*0.1 + xrunif,
          no = x)
-  # min() might change to something related to the data range
-  # possibly use range() to determine the shadow shift
-  # Need to also add some jitter/noise to these points to seperate out repeats of the same value
-  # for factors, need to add another level (smaller than smallest)
-  # need to think about how time is handled as well.
-}
+
+} # close function

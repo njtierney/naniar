d07bb088ed653d68403182806b7c4f821ae0e6c5
Nicholas Tierney <nicholas.tierney@gmail.com>
2015-12-17 08:26:15 +1000

    added roxygen2 to geom_missing
    Tidied up Miles’s geom_missing, adding oxygen, also - ggproto seems to
    only ship with the latest version of ggplot2 from GitHub. At least for
    me. Also, just a note so I don’t forget, if a function depends upon a
    package, add it using `devtools::use_package()`

--- R/geom_missing.R
+++ R/geom_missing.R
@@ -1,23 +1,45 @@
-#library(ggplot2)
-#library(ggmissing)
+#' geom_missing
+#'
+#' \code{geom_missing} does this thing
+#'
+#' @description geom_missing adds a geometry for displaying missingness.
+#'
+#' @note At this stage geom_missing is limited to geom_point
+#'
+#' @note Very first attempt at creating a geom that is compatible with ggplot2.
+#' Data plotting works. Still todo:
+#' manipulate the colour aes so that the colours and legend appear.
+#' fix awful default point sizes.
+#' Warning message if na.rm = T is supplied.
+#'
+#' @note ggplot2 must be installed from GitHub (devtools::install_github("hadley/ggplot2")) to obtain the function `ggproto`
+#'
+#'
+#' @param ? unknown parameters...
+#'
+#' @import ggplot2
+#'
+# this code messes up the documentation
+# library(ggplot2)
+# library(ggmissing)
 #
-#brfss %>%
-#  ggplot(data = .,
+#  ggplot(data = brfss,
 #         aes(x = PHYSHLTH,
 #             y = POORHLTH) ) +
 #  geom_missing()
-#
-# Very first attempt at creating a geom that is compatible with ggplot2. Data plotting works. Still todo:
-# manipulate the colour aes so that the colours and legend appear.
-# fix awful default point sizes.
-# Warning message if na.rm = T is supplied.
+GeomMissing <- ggproto("GeomMissing",
 
+                       Geom,
 
-GeomMissing <- ggproto("GeomMissing", Geom,
                        required_aes = c("x", "y"),
 
-                       default_aes = aes(shape = 19, colour = "black", size = 0.5, fill = NA,
-                                         alpha = NA, stroke = 1.5),
+                       default_aes = aes(shape = 19,
+                                         colour = "black",
+                                         size = 0.5,
+                                         fill = NA,
+                                         alpha = NA,
+                                         stroke = 1.5),
+
                        draw_key = draw_key_point,
 
                        setup_data = function(data, params){
@@ -29,7 +51,6 @@ GeomMissing <- ggproto("GeomMissing", Geom,
 
                        handle_na = function(self, data, params) data,
 
-
                        draw_panel = function(data, panel_scales, coord) {
                          coords <- coord$transform(data, panel_scales)
                          grid::pointsGrob(
@@ -40,9 +61,15 @@ GeomMissing <- ggproto("GeomMissing", Geom,
                        }
 )
 
-geom_missing <- function(mapping = NULL, data = NULL, stat = "identity",
-                              position = "identity", na.rm = FALSE,
-                              show.legend = NA, inherit.aes = TRUE, ...) {
+#' @export
+geom_missing <- function(mapping = NULL,
+                         data = NULL,
+                         stat = "identity",
+                         position = "identity",
+                         na.rm = FALSE,
+                         show.legend = NA,
+                         inherit.aes = TRUE,
+                         ...) {
   layer(
     data = data,
     mapping = mapping,
@@ -58,8 +85,3 @@ geom_missing <- function(mapping = NULL, data = NULL, stat = "identity",
   )
 
 }
-
-
-
-
-

123719c0761f8de71c481af4ac03a337465c196d
Nicholas Tierney <nicholas.tierney@gmail.com>
2016-08-01 15:29:27 +1000

    fix typo that broke the tests

--- tests/testthat/test-missingness-tidiers.R
+++ tests/testthat/test-missingness-tidiers.R
@@ -6,7 +6,7 @@ test_that("percent_missing_* produces a single, numeric number", {
   expect_length(ggmissing::percent_missing_var(airquality), 1)
   testthat::expect_type(ggmissing::percent_missing_var(airquality), "double")
   expect_length(ggmissing::percent_missing_case(airquality), 1)
-  testthat::expect_type(ggmissing::percent_missing_vase(airquality), "double")
+  testthat::expect_type(ggmissing::percent_missing_case(airquality), "double")
 })
 
 test_that("table_missing_* produces a data_frame", {

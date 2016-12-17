148d043ad0dea8595b40e4884a077f85d183253b
Nicholas Tierney <nicholas.tierney@gmail.com>
2016-12-18 02:07:53 +1000

    collect custom plot functions together

---
+++ R/plotting.R
@@ -0,0 +1,99 @@
+# plotting functions
+
+#' gg_missing_case
+#'
+#' This function draws a ggplot plot of the number of missings in each row.
+#' At this point I'm not sure how this plot should be arranged, as it currently looks ugly!
+#'
+#' @param x a dataframe
+#'
+#' @return a ggplot plot depicting the number of missings in a given case
+#' @export
+#'
+#' @examples
+#'
+#' gg_missing_case(airquality)
+#'
+gg_missing_case <- function(x){
+
+  ggplot(data = summary_missing_case(x),
+         aes(y = n_missing,
+             x = case)) +
+    geom_bar(stat="identity", position="dodge", width = 0, colour="grey") +
+    geom_point() +
+    coord_flip() +
+    labs(y = "# Missing",
+         x = "Cases") +
+    theme_minimal()
+
+}
+
+#' gg_missing_var
+#'
+#' This function draws a ggplot plot of the number of missings in each column, rearranged to show which variables have the most missing data.
+#'
+#' @param x a dataframe
+#'
+#' @return a ggplot plot depicting the number of missings in a given column
+#' @export
+#'
+#' @examples
+#'
+#' gg_missing_var(airquality)
+#'
+gg_missing_var <- function(x){
+
+  # get a tidy data frame of the number of missings in each column
+  x %>%
+    summary_missing_var() %>%
+    ggplot(data = .,
+           aes(x = reorder(variable, n_missing),
+               y = n_missing,
+               colour = variable)) +
+    geom_bar(stat="identity", position="dodge", width = 0) +
+    geom_point() +
+    coord_flip() +
+    labs(y = "# Missing",
+         x = "Variables") +
+    theme_minimal() +
+    theme(legend.position = "none")
+
+}
+
+#' gg_missing_which
+#'
+#' \code{gg_missing_which} (need a better name!) produces a set of rectangles that indicate whether there is a missing element in a column or not
+#'
+#' @param x a dataframe
+#'
+#' @return a ggplot plot
+#'
+#' @export
+#'
+#' @examples
+#'
+#' gg_missing_which(airquality)
+#'
+gg_missing_which <- function(x){
+
+  # tell us which columns have missing data
+  airquality %>%
+    purrr::dmap(anyNA) %>%
+    purrr::dmap(function(x) ifelse(x == 0, "complete", "missing")) %>%
+    tidyr::gather(key = "variable",
+                  value = "value") %>%
+    mutate(nheight = 1) %>%
+    ggplot(data = .,
+           aes(x = variable,
+               y = nheight,
+               fill = factor(value))) +
+    geom_tile(colour = "white") +
+    theme_minimal() +
+    scale_fill_grey(name = "") +
+    scale_x_discrete(limits = names(x)) +
+    theme(legend.position = "none") +
+    scale_y_discrete(breaks=c(""),
+                     labels=c("")) +
+    labs(x = "",
+         y = "")
+}

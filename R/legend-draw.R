9b8c6a2a38ac6f6b134e9908ed6ca2e3825673b3
Luke Smith <lukedansmi@sbcglobal.net>
2016-12-22 22:49:46 -0600

    Create legend-draw.R

---
+++ R/legend-draw.R
@@ -0,0 +1,27 @@
+#' Key drawing functions
+#'
+#' Each Geom has an associated function that draws the key when the geom needs
+#' to be displayed in a legend. These are the options built into naniar.
+#'
+#' @return A grid grob.
+#' @param data A single row data frame containing the scaled aesthetics to
+#'   display in this key
+#' @param params A list of additional parameters supplied to the geom.
+#' @param size Width and height of key in mm.
+#' @keywords internal
+#' @name draw_key
+NULL
+
+#' @export
+#' @rdname draw_key
+draw_key_missing_point <- function(data, params, size) {
+  grid::pointsGrob(0.5, 0.5,
+    pch = data$shape,
+    gp = grid::gpar(
+      col = data$colour,
+      fill = alpha(data$fill, data$alpha),
+      fontsize = data$size * .pt + data$stroke * .stroke / 2,
+      lwd = data$stroke * .stroke / 2
+    )
+  )
+}

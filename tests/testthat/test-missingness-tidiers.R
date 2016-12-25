2922e356b31f2b7aacb680fabbdd5578b3a4b551
Nicholas Tierney <nicholas.tierney@gmail.com>
2016-12-25 23:54:04 +1000

    remove :: from testhat calls

--- tests/testthat/test-missingness-tidiers.R
+++ tests/testthat/test-missingness-tidiers.R
@@ -3,18 +3,18 @@ context("missingness_tidiers")
 test_that("percent_missing_* produces a single, numeric number", {
 
   expect_length(percent_missing_df(airquality), 1)
-  testthat::expect_type(percent_missing_df(airquality), "double")
+  expect_type(percent_missing_df(airquality), "double")
 
   expect_length(percent_missing_var(airquality), 1)
-  testthat::expect_type(percent_missing_var(airquality), "double")
+  expect_type(percent_missing_var(airquality), "double")
 
   expect_length(percent_missing_case(airquality), 1)
-  testthat::expect_type(percent_missing_case(airquality), "double")
+  expect_type(percent_missing_case(airquality), "double")
 })
 
 test_that("table_missing_* produces a data_frame", {
-  testthat::expect_is(table_missing_var(airquality), "tbl_df")
-  testthat::expect_is(table_missing_case(airquality), "tbl_df")
+  expect_is(table_missing_var(airquality), "tbl_df")
+  expect_is(table_missing_case(airquality), "tbl_df")
 })
 
 

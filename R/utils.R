dc66a6955f1b96f9616a807e655b5f5a881dcea6
Nicholas Tierney <nicholas.tierney@gmail.com>
2017-05-18 11:47:45 -0700

    add which_na
    which_na is a lightweight way to store position of missing values. Mostly just an idea at this point

---
+++ R/utils.R
@@ -0,0 +1,17 @@
+#' Which rows and cols contain missings?
+#'
+#' Internal function that creates a matrix containing the location of missing
+#' values in a dataframe. This may be used in the future `impl_df` class.
+#'
+#' @param x a dataframe
+#'
+#' @return a matrix with columns "row" and "col", which refer to the row and
+#'     column that identify the position of a missing value in a dataframe
+#'
+#' @examples
+#'
+#' which_na(airquality)
+#'
+which_na <- function(x){
+  which(is.na(x), arr.ind = TRUE)
+}

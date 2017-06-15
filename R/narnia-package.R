2d8a06b6904d8fe877929169e2be4eff295c2903
Nicholas Tierney <nicholas.tierney@gmail.com>
2017-06-15 15:09:57 +1000

    import rlang

--- R/narnia-package.R
+++ R/narnia-package.R
@@ -1,9 +1,14 @@
 #' narnia
 #'
+#' narnia is a package to make it easier to summarise and handle missing values
+#' in R. It strives to do this in a way that is as consistent with tidyverse
+#' principles as possible.
+#'
 #' @name narnia
 #' @docType package
-#' @importFrom magrittr %>%
 #' @import ggplot2
+#' @import rlang
+#' @importFrom magrittr %>%
 NULL
 
 if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))

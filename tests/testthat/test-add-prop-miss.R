3228dc8ba4a2cf34f2798b8b837a7ece92d1d9d5
Nicholas Tierney <nicholas.tierney@gmail.com>
2017-06-29 16:16:30 +0200

    add unquotes variables to the add_* families. as per #61

--- tests/testthat/test-add-prop-miss.R
+++ tests/testthat/test-add-prop-miss.R
@@ -4,9 +4,9 @@ test_df <- data.frame(x = c(NA,2,3),
                       y = c(1,NA,3),
                       z = c(1,2,3))
 
-df_prop_miss <- data.frame(prop_miss = c(1/3,
-                                           1/3,
-                                           0))
+df_prop_miss <- data.frame(prop_miss_all = c(1/3,
+                                             1/3,
+                                             0))
 
 test_df_answer <- dplyr::bind_cols(test_df,df_prop_miss)
 
@@ -15,9 +15,9 @@ test_that("add_prop_miss adds a column",{
   expect_equal(ncol(test_df) + 1, ncol(add_prop_miss(test_df)))
 })
 
-test_that("add_prop adds a column named 'prop_miss'",{
+test_that("add_prop adds a column named 'prop_miss_all'",{
   expect_equal(names(add_prop_miss(test_df)),
-               c(names(test_df),"prop_miss"))
+               c(names(test_df),"prop_miss_all"))
 })
 
 test_that("add_prop_miss returns the correct number",{

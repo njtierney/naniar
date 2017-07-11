e89894c521860a093fe3503d52a00c3e2aff0b63
Nicholas Tierney <nicholas.tierney@gmail.com>
2017-07-11 14:19:28 +0200

    add all the globalVariables to remove NOTE

--- R/narnia-package.R
+++ R/narnia-package.R
@@ -11,3 +11,25 @@
 NULL
 
 if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
+globalVariables(
+  c(
+    ".temp",
+    ".temp_label",
+    "rows",
+    "..missing..",
+    "n_missing",
+    "case",
+    "variable",
+    "value",
+    "span_counter",
+    "n",
+    "n_missing",
+    "nheight",
+    "percent",
+    "n_missing_in_case",
+    "values",
+    "n_missing_in_case",
+    "n_missing_in_var",
+    "n_vars"
+    )
+  )

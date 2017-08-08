20669a234b0fad7ad229b014c5ed9975e00d985b
Nicholas Tierney <nicholas.tierney@gmail.com>
2017-08-09 01:21:18 +1000

    appease appveyor gods by ~~sacrificing a goat~~ adding `skip_on_appveyor` for vdiffr plots.

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -9,6 +9,7 @@ geom_miss_point_plot <-
 
 test_that("geom_miss_point works",{
   skip_on_cran()
+  skip_on_appveyor()
   vdiffr::expect_doppelganger("geom_miss_point", geom_miss_point_plot)
 })
 
@@ -17,6 +18,7 @@ gg_miss_case_plot <- gg_miss_case(airquality)
 
 test_that("gg_miss_case_works",{
   skip_on_cran()
+  skip_on_appveyor()
   vdiffr::expect_doppelganger("gg_miss_case",
                               gg_miss_case_plot)
 })
@@ -25,6 +27,7 @@ gg_miss_var_plot <- gg_miss_var(airquality)
 
 test_that("gg_miss_var_works",{
   skip_on_cran()
+  skip_on_appveyor()
   vdiffr::expect_doppelganger("gg_miss_var",
                               gg_miss_var_plot)
 })
@@ -33,6 +36,7 @@ gg_miss_which_plot <- gg_miss_which(airquality)
 
 test_that("gg_miss_which_works",{
   skip_on_cran()
+  skip_on_appveyor()
   vdiffr::expect_doppelganger("gg_miss_which",
                               gg_miss_which_plot)
 })
@@ -41,6 +45,7 @@ gg_miss_fct_plot <- gg_miss_fct(riskfactors, marital)
 
 test_that("gg_miss_fct works",{
   skip_on_cran()
+  skip_on_appveyor()
   vdiffr::expect_doppelganger("gg_miss_fct",
                               gg_miss_fct_plot)
 })
@@ -49,6 +54,7 @@ gg_miss_span_plot <- gg_miss_span(pedestrian, hourly_counts, 4000)
 
 test_that("gg_miss_span works",{
   skip_on_cran()
+  skip_on_appveyor()
   vdiffr::expect_doppelganger("gg_miss_span",
                               gg_miss_span_plot)
 })

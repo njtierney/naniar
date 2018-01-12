059cf479e9f4b9ce75760b1cb453a69d65fe0aeb
Nicholas Tierney <nicholas.tierney@gmail.com>
2018-01-12 16:49:15 +1100

    finished first run of test for the scoped variants

--- tests/testthat/test-replace-to-na-scoped-var.R
+++ tests/testthat/test-replace-to-na-scoped-var.R
@@ -4,39 +4,64 @@ test_that("empty call provides an error", {
   df <- tibble::tibble(x = c("A", NA))
   expect_error(replace_to_na_where(df))
 })
-#
-# test_that("suggested single values are replaced with NAs", {
-#   df <- tibble::tibble(x = c(1, NA, -99))
-#   out <- replace_to_na(df, list(x = -99))
-#   expect_equal(out$x, c(1, NA, NA))
-# })
-#
-# test_that("suggested multiple values are replaced with NAs", {
-#   df <- tibble::tibble(x = c(1, NA, -99, -98))
-#   out <- replace_to_na(df, list(x = c(-98, -99)))
-#   expect_equal(out$x, c(1, NA, NA, NA))
-# })
-#
-# dat_ms <- tibble::tribble(~x,  ~y,    ~z,
-#                           1,   "A",   -100,
-#                           3,   "N/A", -99,
-#                           NA,  NA,    -98,
-#                           -99, "E",   -101,
-#                           -98, "F",   -1)
-#
-# test_that("only specified columns are affected",{
-#   df <- dat_ms
-#   out <- replace_to_na(df, list(x = c(-99)))
-#   expect_equal(out$x, c(1,3,NA,NA,-98))
-#   expect_equal(out$z, c(-100, -99, -98, -101, -1))
-# })
-#
-# test_that("works for multiple columns",{
-#   df <- dat_ms
-#   out <- replace_to_na(df, list(x = c(-99, -98),
-#                                 y = c("N/A"),
-#                                 z = c(-1, -99)))
-#   expect_equal(out$x, c(1,3,NA,NA,NA))
-#   expect_equal(out$y, c("A",NA,NA,"E","F"))
-#   expect_equal(out$z, c(-100, NA, -98, -101, NA))
-# })
+
+test_that("suggested single values are replaced with NAs", {
+  df <- tibble::tibble(x = c(1, NA, -99))
+  out <- replace_to_na_where(df, ~.x == -99)
+  expect_equal(out$x, c(1, NA, NA))
+})
+
+test_that("suggested multiple values are replaced with NAs", {
+  df <- tibble::tibble(x = c(1, NA, -99, -98))
+  out <- replace_to_na_where(df, ~.x %in% c(-98, -99))
+  expect_equal(out$x, c(1, NA, NA, NA))
+})
+
+dat_ms <- tibble::tribble(~x,  ~y,    ~z,
+                          1,   "A",   -100,
+                          3,   "N/A", -99,
+                          NA,  NA,    -98,
+                          -99, "E",   -101,
+                          -98, "F",   -1)
+
+test_that("all columns are affected by _where",{
+  df <- dat_ms
+  out <- replace_to_na_where(df, ~.x == -99)
+  expect_equal(out$x, c(1,3,NA,NA,-98))
+  expect_equal(out$z, c(-100, NA, -98, -101, -1))
+})
+
+test_that("1 column selection affected by _at",{
+  df <- dat_ms
+  out <- replace_to_na_at(data = df,
+                          .vars = "x",
+                          .funs = ~.x == -99)
+  expect_equal(out$x, c(1,3,NA,NA,-98))
+  expect_equal(out$z, c(-100, -99, -98, -101, -1))
+})
+
+test_that("2 column selection affected by _at",{
+  df <- dat_ms
+  out <- replace_to_na_at(data = df,
+                          .vars = c("x","z"),
+                          .funs = ~.x == -99)
+  expect_equal(out$x, c(1,3,NA,NA,-98))
+  expect_equal(out$z, c(-100, NA, -98, -101, -1))
+})
+
+dat_ms <- tibble::tribble(~x,  ~y,    ~z,
+                          1L,   "A",   -100,
+                          3L,   "N/A", -99,
+                          NA,  NA,    -98,
+                          -99L, "E",   -101,
+                          -98L, "F",   -1)
+
+
+test_that("columns are affected by _if",{
+  df <- dat_ms
+  out <- replace_to_na_if(data = df,
+                          .predicate = is.integer,
+                          .funs = ~.x == -99)
+  expect_equal(out$x, c(1,3,NA,NA,-98))
+  expect_equal(out$z, c(-100, -99, -98, -101, -1))
+})

1975373819a126fede632d93ab98589542335aea
Nicholas Tierney <nicholas.tierney@gmail.com>
2018-01-19 14:06:08 +1100

    fix data that had factors for lat/long/year - close #110

--- data-raw/create-data-oceanbuoys.R
+++ data-raw/create-data-oceanbuoys.R
@@ -1,14 +1,19 @@
 # modify the tao data from the MissingDataGUI dataset
-
-library(naniar)
+# library(MissingDataGUI)
 library(dplyr)
+
+# downloaded from https://github.com/chxy/MissingDataGUI/blob/master/data/tao.rda
+# as I could not install MissingDataGUI
+# load("~/Downloads/tao.rda")
 
 oceanbuoys <- tao %>%
   rename(sea_temp_c = sea.surface.temp,
-         air_temp_c = air.temp,
+                air_temp_c = air.temp,
          wind_ew = uwind,
          wind_ns = vwind) %>%
-  as_tibble()
+  as_tibble() %>%
+  # convert the factors in a sane way
+  mutate_if(is.factor, .funs = function(x) as.numeric(as.character(x)))
 
 devtools::use_data(oceanbuoys, overwrite = TRUE)
 # then delete the tao dataset

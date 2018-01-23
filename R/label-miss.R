938a15d1bd45dde688b5f36f597ee5b359b6ef03
Nicholas Tierney <nicholas.tierney@gmail.com>
2018-01-23 14:17:40 +1100

    use integer not TRUE or FALSE in the shadow factor levels

--- R/label-miss.R
+++ R/label-miss.R
@@ -19,9 +19,12 @@ label_shadow_matrix <- function(x) {
   if (length(x) == 0) {
     stop("Input is of length 0, please check your inputs.", call. = FALSE)
   } else{
-    factor(is.na(x),
-           levels = c(FALSE, TRUE),
-           labels = c("!NA", "NA"))
+    factor(
+      # is.na(x),
+      as.numeric(is.na(x)),
+      # levels = c(FALSE, TRUE),
+      levels = c(0L, 1L),
+      labels = c("!NA", "NA"))
   }
 }
 

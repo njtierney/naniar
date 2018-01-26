a27942f68f072cf64ae45d1cf1cf21447c548272
Nicholas Tierney <nicholas.tierney@gmail.com>
2018-01-26 16:39:27 +1100

    tests for gg-miss-case and gg-miss-var

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -13,6 +13,7 @@ test_that("geom_miss_point works",{
   vdiffr::expect_doppelganger("geom_miss_point", geom_miss_point_plot)
 })
 
+# gg_miss_case plots -----------------------------------------------------------
 
 gg_miss_case_plot <- gg_miss_case(airquality)
 
@@ -23,6 +24,37 @@ test_that("gg_miss_case_works",{
                               gg_miss_case_plot)
 })
 
+gg_miss_case_plot_group <- gg_miss_case(airquality, group = Month)
+
+test_that("gg_miss_case_group_works",{
+  skip_on_cran()
+  skip_on_appveyor()
+  vdiffr::expect_doppelganger("gg_miss_case_group",
+                              gg_miss_case_plot_group)
+})
+
+gg_miss_case_plot_sort <- gg_miss_case(airquality, order_cases = TRUE)
+
+test_that("gg_miss_case_sort_works",{
+  skip_on_cran()
+  skip_on_appveyor()
+  vdiffr::expect_doppelganger("gg_miss_case_sort",
+                              gg_miss_case_plot_sort)
+})
+
+gg_miss_case_plot_order_group_sort <- gg_miss_case(airquality,
+                                                   group = Month,
+                                                   order_cases = TRUE)
+
+test_that("gg_miss_case_group_and_sort_works",{
+  skip_on_cran()
+  skip_on_appveyor()
+  vdiffr::expect_doppelganger("gg_miss_case_group_and_sort",
+                              gg_miss_case_plot_order_group_sort)
+})
+
+# gg_miss_var plots ------------------------------------------------------------
+
 gg_miss_var_plot <- gg_miss_var(airquality)
 
 test_that("gg_miss_var_works",{
@@ -30,6 +62,37 @@ test_that("gg_miss_var_works",{
   skip_on_appveyor()
   vdiffr::expect_doppelganger("gg_miss_var",
                               gg_miss_var_plot)
+})
+
+gg_miss_var_plot_group <- gg_miss_var(airquality, group = Month)
+
+test_that("gg_miss_var_group_works",{
+  skip_on_cran()
+  skip_on_appveyor()
+  vdiffr::expect_doppelganger("gg_miss_var_plot_group",
+                              gg_miss_var_plot_group)
+})
+
+
+gg_miss_var_plot_pct <- gg_miss_var(airquality,
+                                    show_pct = TRUE)
+
+test_that("gg_miss_var_pct_works",{
+  skip_on_cran()
+  skip_on_appveyor()
+  vdiffr::expect_doppelganger("gg_miss_var_pct",
+                              gg_miss_var_plot_pct)
+})
+
+gg_miss_var_plot_group_pct <- gg_miss_var(airquality,
+                                      group = Month,
+                                      show_pct = TRUE)
+
+test_that("gg_miss_var_works",{
+  skip_on_cran()
+  skip_on_appveyor()
+  vdiffr::expect_doppelganger("gg_miss_var_group_pct",
+                              gg_miss_var_plot_group_pct)
 })
 
 gg_miss_which_plot <- gg_miss_which(airquality)

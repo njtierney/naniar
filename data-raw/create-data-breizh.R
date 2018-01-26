bef29736aea9889f4533d9db62a55e3717ebb9f8
Colin <colin@thinkr.fr>
2018-01-26 09:02:57 +0100

    new dataset

---
+++ data-raw/create-data-breizh.R
@@ -0,0 +1,11 @@
+# Download the file
+
+download.file(url = "https://breizh-sba.opendatasoft.com/explore/dataset/marches-publics-collectivites-bretonnes/download/?format=csv&timezone=Europe/Berlin&use_labels_for_header=true", destfile = "breizh.csv")
+breizh <- read.csv2("breizh.csv", stringsAsFactors = FALSE)
+
+# Remove non ascii char so CRAN will be happy \o/ 
+names(breizh) <- proustr::unacent(names(breizh))
+breizh <- purrr::map_df(breizh, proustr::unacent)
+
+# Save
+usethis::use_data(breizh, overwrite = TRUE)

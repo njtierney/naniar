63afe1798acecc516244a9d590e9051ca88a6905
Nicholas Tierney <nicholas.tierney@gmail.com>
2018-01-28 00:44:22 +1100

    added tests for infinite value shadhow shift method

---
+++ tests/testthat/test-shadow-shift-inf.R
@@ -0,0 +1,11 @@
+context("infinite values for shadow shift")
+
+df_inf <-  data.frame(x = c(-Inf,rnorm(2), NA, Inf))
+
+test_that("missing values are replaced in shadow shift",{
+  expect_false(anyNA(shadow_shift(df_inf$x)))
+})
+
+test_that("infinite values are maintained in shadow shift",{
+  expect_equal(sum(is.infinite(shadow_shift(df_inf$x))), 2)
+})

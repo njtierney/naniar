2d7a18f6f183cfb09fbf55d979d2144da0ac85ef
Romain Francois <romain@purrple.cat>
2018-03-22 15:31:11 +0100

    use `isnan` as this is what R's is.na does anyway

--- src/naniar.h
+++ src/naniar.h
@@ -12,19 +12,9 @@ namespace naniar{
     return Rcpp::Vector<RTYPE>::is_na(x) ;
   }
 
-  typedef union {
-    double value;
-    unsigned int word[2];
-  } ieee_double;
-
   template <>
   inline bool isna<REALSXP>( double x){
-    if (isnan(x)) {
-      ieee_double y;
-      y.value = x;
-      return (y.word[0] == 1954);
-    }
-    return false ;
+    return isnan(x) ;
   }
 
 }

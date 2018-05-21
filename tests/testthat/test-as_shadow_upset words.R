521b094021c8d0f0a9e72970eece3a209a15553c
Nicholas Tierney <nicholas.tierney@gmail.com>
2018-05-21 13:09:23 +1000

    add example usage of as_shadow_upset in README and vignette

---
+++ tests/testthat/test-as_shadow_upset words.R
@@ -0,0 +1,28 @@
+context("as_shadow_upset words")
+
+
+
+test_that("as_shadow_upset returns a data.frame",{
+  expect_s3_class(as_shadow_upset(airquality),
+                  "data.frame")
+})
+
+test_that("as_shadow_upset errors when given non dataframe or 0 entry",{
+  expect_error(as_shadow_upset(0))
+  expect_error(as_shadow_upset("a"))
+  expect_error(as_shadow_upset(matrix(airquality)))
+  expect_error(as_shadow_upset(NULL))
+})
+
+test_that("The number of rows are the same after using as_shadow_upset",{
+  expect_equal(nrow(as_shadow_upset(airquality)),nrow(airquality))
+})
+
+test_that("The number of columns are the same after using bind_shadow",{
+  expect_equal(ncol(as_shadow_upset(airquality)),ncol(airquality))
+})
+
+aq_u <- as_shadow_upset(airquality)
+test_that("as_shadow_upset returns things of type integer", {
+  expect_true(all(purrr::map_chr(aq_u,class) == "integer"))
+})

4b99569063974f831e05325655086e5a9f7aed21
Nicholas Tierney <nicholas.tierney@gmail.com>
2018-06-21 12:10:29 +1000

    update docs to reflect changes in gg_miss_upset

--- R/gg-miss-upset.R
+++ R/gg-miss-upset.R
@@ -1,15 +1,15 @@
 #' Plot the pattern of missingness using an upset plot.
 #'
-#' Upset plots are a way of visualising common sets, gg_miss_upset shows the
+#' Upset plots are a way of visualising common sets, `gg_miss_upset`` shows the
 #'   number of missing values for each of the sets of data. The default option
-#'   is to use 5 sets and up  to 40 interactions. Setting nsets to 5 means
-#'   to look at 5 variables and their combinations - the number of combinations
-#'   or rather `intersections` is controlled by `nintersects` - if there are 40
-#'   intersections, this means that 40 combinations of the variables will be
-#'   explored. The number of nets and intersections can be changed
-#'   by passing arguments `nsets = 10` to look at 10 sets of variables, and
-#'   `nintersects = 50` to look at 50 intersections. Setting `nintersects` to
-#'   `NA` it will plot all sets and all intersections.
+#'   of `gg_miss_upset` is taken from `UpSetR::upset` - which is to use up to 5
+#'   sets and up to 40 interactions. Here, setting `nsets = 5` means to look at
+#'   5 variables and their combinations. The number of combinations or rather
+#'   `intersections` is controlled by `nintersects`. If there are 40
+#'   intersections, there will be 40 combinations of variables explored. The
+#'   number of sets and intersections can be changed by passing arguments
+#'   `nsets = 10` to look at 10 sets of variables, and `nintersects = 50` to
+#'   look at 50 intersections.
 #'
 #' @param data data.frame
 #' @param ... arguments to pass to upset plot - see `?UpSetR::upset`

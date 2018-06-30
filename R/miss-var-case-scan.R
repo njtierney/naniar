f6483ef1b7c0ff3555dc9b8cfb80cb6110e24e04
Nicholas Tierney <nicholas.tierney@gmail.com>
2018-06-30 19:55:28 +1000

    first draft of miss_var_scan

--- R/miss-var-case-scan.R
+++ R/miss-var-case-scan.R
@@ -1,64 +1,46 @@
-# library(naniar)
-# library(tidyverse)
-#
-# dat_ms <- tibble::tribble(~x,  ~y,    ~z,
-#                           3,   "N/A",  -99,
-#                           -99, "E",    97,
-#                           -98, "F",    97)
-#
-# n_match <- function(x, search){
-#   sum(grepl(search, x))
-# }
-#
-# n_match(dat_ms, -99)
-#
-# dat_ms %>%
-#   map(n_match,-99)
-#
-# dat_ms %>%
-#   map(n_match,-98)
-#
-# dat_ms %>%
-#   map(n_match,"N/A")
-#
-# # ideal
-# dat_ms %>%
-#   map(n_match,
-#       list("N/A",
-#            -99))
-#
-# v1 <- dat_ms %>%
-#   map(n_match,-99) %>%
-#   t() %>% t()
-#
-# v2 <- dat_ms %>%
-#   map(n_match,-98) %>%
-#   t() %>% t()
-#
-# v3 <- dat_ms %>%
-#   map(n_match,"N/A") %>%
-#   t() %>% t()
-#
-# my_mat <- cbind(v1, v2, v3)
-#
-# colnames(my_mat) <- c("-99", "-98", "N/A")
-#
-# rownames(my_mat)
-#
-# as_tibble(my_mat) %>%
-#   unnest() %>%
-#   rownames_to_column()
-#
-# dat_ms %>%
-#   map(n_match, list(-99,
-#                    -98))
-#
-# n_match(list(-99,-98), dat_ms)
-#
-# dat_ms %>%
-#   summarise_all(~sum(grepl(search,.x), na.rm = TRUE))
-#
-# dat_ms %>%
-#   purrr::map_df(~length(grep(paste0("N/A", collapse ="|"),x = .))) %>%
-#   tidyr::gather(key = "Variable",
-#                 value = "n")
+#' Scan variables for particular possible missings
+#'
+#' @param data data.frame
+#' @param scan list of values to scan
+#'
+#' @return summary data.frame of variables and how many scanned vars
+#' @export
+#'
+#' @examples
+#'
+#' dat_ms <- tribble(~x,  ~y,    ~z,
+#'                   3,   "N/A",  -99,
+#'                   -99, "E",    97,
+#'                   -98, "F",    97)
+#'
+#' miss_var_scan(dat_ms,
+#'               list("N/A",
+#'                    -99))
+#'
+#' miss_var_scan(dat_ms,
+#'               list("N/A",
+#'                    -99,
+#'                    -98))
+miss_var_scan <- function(data, scan){
+
+  n_match <- function(x, scan){
+    stopifnot(is.list(scan))
+    map(scan, ~sum(grepl(., x)))
+  }
+
+  purrr::map(data,
+             n_match,
+             scan) %>%
+    purrr::transpose() %>%
+    purrr::map(transpose)  %>%
+    dplyr::bind_cols() %>%
+    dplyr::unnest() %>%
+    rlang::set_names(scan) %>%
+    dplyr::mutate(variables = names(data)) %>%
+    janitor::adorn_totals(where = c("col")) %>%
+    dplyr::select(variables,
+                  Total,
+           dplyr::everything()) %>%
+    dplyr::rename(total = Total)
+
+}

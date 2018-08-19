babebcd614f3853d2a889d6a13975832be1a2552
Nicholas Tierney <nicholas.tierney@gmail.com>
2018-08-19 20:31:30 +1000

    add tests for how impute_below works with shadow data

--- tests/testthat/test-impute-below.R
+++ tests/testthat/test-impute-below.R
@@ -103,3 +103,12 @@ test_that("impute_below_all jitter makes shifts bigger",{
 
   )
 })
+
+aq_s <- bind_shadow(airquality)
+
+test_that("impute_below works with bind_shadow", {
+  expect_is(impute_below(aq_s$Ozone), "numeric")
+  expect_is(impute_below_all(aq_s), "data.frame")
+  expect_is(impute_below_at(aq_s, dplyr::vars(Ozone, Solar.R)), "data.frame")
+  expect_is(impute_below_if(aq_s, is.numeric), "data.frame")
+})

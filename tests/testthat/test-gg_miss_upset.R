3d8ce0b1930690c1ed0fcefc6405870424c225d0
Nicholas Tierney <nicholas.tierney@gmail.com>
2019-10-23 12:38:41 +1100

    skip gg_miss_upset on travis. only run locally?

--- tests/testthat/test-gg_miss_upset.R
+++ tests/testthat/test-gg_miss_upset.R
@@ -5,6 +5,7 @@ gg_miss_upset_plot <- gg_miss_upset(riskfactors)
 test_that("gg_miss_upset works",{
   skip_on_cran()
   skip_on_appveyor()
+  skip_on_travis()
   vdiffr::expect_doppelganger("gg_miss_upset",
                               gg_miss_upset_plot)
 })

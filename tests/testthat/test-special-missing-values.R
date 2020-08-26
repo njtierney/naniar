dc7f73036f026abc6632a7fbf5d5981009f7ce45
Nicholas Tierney <nicholas.tierney@gmail.com>
2020-08-26 10:44:58 +1000

    isolate class issue

--- tests/testthat/test-special-missing-values.R
+++ tests/testthat/test-special-missing-values.R
@@ -142,7 +142,6 @@ test_that("special missings are the same for grouped and ungrouped data", {
 
 
 test_that("special missings class is maintained for grouped and ungrouped data", {
-  skip_on_ci()
   expect_equal(class(aq_grouped_recoded$Ozone_NA),
                class(aq_recoded$Ozone_NA))
 })

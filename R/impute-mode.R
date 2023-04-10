e1166b9a0d7f6dcd38075617d160b9506c120a28
njtierney <nicholas.tierney@gmail.com>
2023-04-10 18:42:59 +1000

    minor doc improvemments and add tests

--- R/impute-mode.R
+++ R/impute-mode.R
@@ -20,6 +20,10 @@ the_mode <- function(x, na.rm = FALSE) {
 #'
 #' @param x vector
 #'
+#' This can be useful if you are imputing specific values, however we would
+#'   generally recommend to impute using other model based approaches. See
+#'   the `simputation` package, for example [simputation::impute_lm()].
+#'
 #' @return vector with mode values replaced
 #' @export
 #' @name impute_mode
@@ -31,6 +35,29 @@ the_mode <- function(x, na.rm = FALSE) {
 #' vec[sample(1:10, 3)] <- NA
 #'
 #' impute_mode(vec)
+#'
+#' library(dplyr)
+#'
+#' dat <- tibble(
+#'   num = rnorm(10),
+#'   int = rpois(10, 5),
+#'   fct = factor(LETTERS[1:10])
+#' ) %>%
+#'   mutate(
+#'     across(
+#'       everything(),
+#'       \(x) set_prop_miss(x, prop = 0.25)
+#'     )
+#'   )
+#'
+#' dat
+#'
+#' dat %>%
+#'   nabular() %>%
+#'   mutate(
+#'     num = impute_mode(num)
+#'   )
+#'
 #'
 impute_mode <- function(x) UseMethod("impute_mode")
 
@@ -66,88 +93,4 @@ impute_mode.factor <- function(x){
   x[is.na(x)] <- i_mode(x)
 
   x
-}
-
-#' Scoped variants of `impute_mode`
-#'
-#' `impute_mode` imputes the mode for a vector. To get it to work on all
-#'   variables, use `impute_mode_all`. To only impute variables
-#'   that satisfy a specific condition, use the scoped variants,
-#'   `impute_below_at`, and `impute_below_if`. To use `_at` effectively,
-#'   you must know that `_at`` affects variables selected with a character
-#'   vector, or with `vars()`.
-#'
-#' @param .tbl a data.frame
-#' @param .vars variables to impute
-#' @param .predicate variables to impute
-#' @name scoped-impute_mode
-#'
-#' @return an dataset with values imputed
-#' @export
-#'
-#' @examples
-#' # select variables starting with a particular string.
-#' library(dplyr)
-#' impute_mode_all(airquality)
-#'
-#' impute_mode_at(airquality,
-#'                .vars = c("Ozone", "Solar.R"))
-#'
-#' impute_mode_at(airquality,
-#'                 .vars = vars(Ozone))
-#'
-#' impute_mode_if(airquality,
-#'                 .predicate = is.numeric)
-#'
-#' \dontrun{
-#' library(ggplot2)
-#' airquality %>%
-#'   bind_shadow() %>%
-#'   impute_mode_all() %>%
-#'   add_label_shadow() %>%
-#'   ggplot(aes(x = Ozone,
-#'              y = Solar.R,
-#'              colour = any_missing)) +
-#'          geom_point()
-#' }
-#'
-impute_mode_all <- function(.tbl){
-
-  test_if_dataframe(.tbl)
-
-  test_if_null(.tbl)
-
-  dplyr::mutate_all(.tbl = .tbl,
-                    .funs = impute_mode)
-
-}
-
-#' @export
-#' @rdname scoped-impute_mode
-impute_mode_at <- function(.tbl,
-                             .vars){
-
-  test_if_dataframe(.tbl)
-
-  test_if_null(.tbl)
-
-  dplyr::mutate_at(.tbl = .tbl,
-                   .vars = .vars,
-                   .funs = impute_mode)
-
-}
-
-#' @export
-#' @rdname scoped-impute_mode
-impute_mode_if <- function(.tbl,
-                             .predicate){
-
-  test_if_dataframe(.tbl)
-
-  test_if_null(.tbl)
-
-  dplyr::mutate_if(.tbl = .tbl,
-                   .predicate = .predicate,
-                   .funs = impute_mode)
-
 }
---
+++ tests/testthat/test-impute-mode.R
@@ -0,0 +1,44 @@
+vec <- rnorm(10)
+
+vec[sample(1:10, 3)] <- NA
+
+vec2 <- vec
+
+vec2[is.na(vec)] <- the_mode(vec, na.rm = TRUE)
+
+aq_shadow <- nabular(airquality)
+
+test_that("impute_mode works", {
+  expect_equal(impute_mode(vec), vec2)
+})
+
+## impute_mode_across --------------------------------------------------------
+library(dplyr)
+test_that("impute_mode works with across", {
+  expect_false(
+    mutate(airquality,
+           across(where(is.numeric), \(x) impute_mode(x))) %>%
+      all_na()
+  )
+})
+
+test_that("impute_mode works with across and nabular", {
+  expect_false(
+    mutate(aq_shadow,
+           across(where(is.numeric), \(x) impute_mode(x))) %>%
+      all_na()
+  )
+})
+
+test_that("impute_mode retains proper shadow values when used with across", {
+  expect_equal(
+    unbind_data(
+      mutate(aq_shadow,
+             across(where(is.numeric), \(x) impute_mode(x)))
+    ),
+    unbind_data(
+      aq_shadow
+    )
+  )
+})
+
